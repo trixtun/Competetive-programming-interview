e6972042e5d9d01209cd686f7dabc3f48a03f7fe
Tushar Takshak <66051224+trixtun@users.noreply.github.com>
2020-12-29 20:25:12 +0530

    Create string-formatting.py

---
+++ string-formatting.py
@@ -0,0 +1,14 @@
+#Problem link: https://bit.ly/3rC9ELj
+
+
+
+
+def print_formatted(number):
+    width = len("{0:b}".format(number))
+
+    for i in range(1, number + 1):
+        print("{0:{w}d} {0:{w}o} {0:{w}X} {0:{w}b}".format(i, w = width))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
