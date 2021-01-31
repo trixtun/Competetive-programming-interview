d438f941f75567752335881533be7cb3b562c45b
Tushar Takshak <66051224+trixtun@users.noreply.github.com>
2021-01-31 09:58:33 +0530

    Create ReverseString.py

---
+++ ReverseString.py
@@ -0,0 +1,12 @@
+# s = "Hi My name is Tushar"
+# l1 = list(s)
+# l2 = []
+# for i in range(20):
+#     l2.append(l1[19-i])
+
+# listToStr = ''.join([str(elem) for elem in l2])     
+# print(listToStr)      
+
+
+txt = "Hello World"[::-1]
+print(txt)
