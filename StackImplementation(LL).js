efef7ff21f639ab90dd45d6947efd99c3a49bcb4
Tushar Takshak <66051224+trixtun@users.noreply.github.com>
2021-02-26 10:17:14 +0530

    Update StackImplementation(LL).js

--- StackImplementation(LL).js
+++ StackImplementation(LL).js
@@ -12,19 +12,40 @@ class Stack {
     this.length = 0;
   }
   peek() {
+    
+    return this.top;
+    
   }
   push(value){
+    
+    const newNode = new Node(value);
+    if(this.length == 0) {
+          this.top = newNode;
+          this.bottom = newNode;
+    } else {
+        const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
   }
   pop(){
+    
+    if(!this.top) {
+    return null;
+    }
+    if(this.top === this.bottom){
+    this.bottom = null;
+    
+    }
+    const holdingPointer = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return this;
   }
   //isEmpty
 }
 
 const myStack = new Stack();
 
-
-
-
-//Discord
-//Udemy
-//google
